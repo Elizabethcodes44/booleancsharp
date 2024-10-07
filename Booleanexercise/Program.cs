ba885b75b8ff367405a70fe87cdd043e5ee4bf37
Elizabeth <lizbeepmc@gmail.com>
2024-10-07 15:36:51 +0300

    rrr

--- Booleanexercise/Program.cs
+++ Booleanexercise/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+
+string myValue1 = "a";
+string myValue2 = "A";
+Console.WriteLine(myValue1.Trim().ToLower() == myValue2.Trim().ToLower());
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue3 = "a";
+Console.WriteLine(myValue3 != "a");
+
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+
+//Contains(), StartsWith(), and EndsWith().
+//methods that returns a bolean
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
---
+++ conditionalchallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿Random coin = new Random();
+int roll = coin.Next(1, 3);
+Console.WriteLine(roll);
+Console.WriteLine($"Score: {(roll == 1 ? "Head" : "Tail")}");
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
