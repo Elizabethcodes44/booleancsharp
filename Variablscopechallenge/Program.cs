5f633f4dd5adeb1b06f81fefdaad56d278a5eb5e
Elizabeth <lizbeepmc@gmail.com>
2024-10-08 14:26:11 +0300

    switch statements

--- Variablscopechallenge/Program.cs
+++ Variablscopechallenge/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+int total = 0;
+foreach (int number in numbers)
+   {
+    if (number == 42)
+    Console.WriteLine("set contains 42");
+    total += number;
+    
+   }
+
+  
+
+Console.WriteLine($"Total: {total}");
